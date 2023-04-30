cf6b35a3ad3f6ceffb10837bbcd206da03b4d0a5
DennyBui <dbuidmd2018@gmail.com>
2023-04-29 17:38:43 -0700

    added a foreach in main.js

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 fetch("http://localhost:3000/characters")
 .then(resp => resp.json())
-.then(json => console.log(json))
+.then(characters => {
+    const container = document.querySelector('ul#genshinCharacter')
+    characters.forEach(character => {
+        //name, element, rarity
+        //create an element to hold the name. maybe h2 or h3
+        // create an element to hold element and rarity
+        //create a list element
+        //append elements to list item
+        //append list item to container
+        //add images to db.json
+    })
+})

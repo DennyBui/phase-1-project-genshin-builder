516a3633dbc832831b7bc47cf110f91dc5e2f4ad
DennyBui <dbuidmd2018@gmail.com>
2023-05-05 13:41:56 -0700

    added artifact images and description

--- main.js
+++ main.js
@@ -1,24 +1,35 @@
-let buildCharacter = false;
 function createCardElement(character) {
   let card = document.createElement('div')
   card.classList.add('card')
   let h2 = document.createElement('h2')
   h2.textContent = character.name
   let p = document.createElement('p')
-  p.textContent = `${character.recommended_artifacts} Recommended Artifact`
+  p.textContent = "Artifact"
   let img = document.createElement('img')
   img.src = character.image
   img.classList.add("character-portrait")
   let button = document.createElement('button')
+  button.textContent = "Build Character"
   button.addEventListener("click", () => {
-    p.textContent = `${recommended_artifacts} Recommended Artifact`;
-    
+    p.textContent = "Recommended"
+    character.recommendedArtifacts.forEach(artifact => {
+      let artifactImg = document.createElement("img")
+      artifactImg.src = artifact['artifact-image']
+      artifactImg.alt = artifact.name
+      artifactImg.classList.add('recommended-artifact-image')
+      card.appendChild(artifactImg)
+      let artifactDesc = document.createElement('p')
+      artifactDesc.textContent = artifact.description
+      artifactDesc.classList.add('recommended-artifact-description')
+      card.appendChild(artifactDesc)
+    })  
   })
   button.classList.add('build-character')
   button.id = character.id
   card.append(h2, img, p, button)
   document.getElementById('cards-container').appendChild(card)
 }
+
 document.addEventListener("DOMContentLoaded", () => {
   fetch('http://localhost:3000/characters')
   .then(res => res.json())

b809d11925fd42359d57ed38729b1e3f07548bce
DennyBui <dbuidmd2018@gmail.com>
2023-05-05 13:00:01 -0700

    created a card element

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+let buildCharacter = false;
+function createCardElement(character) {
+  let card = document.createElement('div')
+  card.classList.add('card')
+  let h2 = document.createElement('h2')
+  h2.textContent = character.name
+}
 document.addEventListener("DOMContentLoaded", () => {
   fetch('http://localhost:3000/characters')
   .then(res => res.json())

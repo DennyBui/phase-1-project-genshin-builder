0ee38e4a998dcc6da810f7fe87b9b6f6a8cf44c2
DennyBui <dbuidmd2018@gmail.com>
2023-05-05 13:13:35 -0700

    got images to work

--- main.js
+++ main.js
@@ -4,6 +4,20 @@ function createCardElement(character) {
   card.classList.add('card')
   let h2 = document.createElement('h2')
   h2.textContent = character.name
+  let p = document.createElement('p')
+  p.textContent = `${character.recommended_artifacts} Recommended Artifact`
+  let img = document.createElement('img')
+  img.src = character.image
+  img.classList.add("character-portrait")
+  let button = document.createElement('button')
+  button.addEventListener("click", () => {
+    p.textContent = `${recommended_artifacts} Recommended Artifact`;
+    
+  })
+  button.classList.add('build-character')
+  button.id = character.id
+  card.append(h2, img, p, button)
+  document.getElementById('cards-container').appendChild(card)
 }
 document.addEventListener("DOMContentLoaded", () => {
   fetch('http://localhost:3000/characters')
